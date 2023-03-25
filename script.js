8d3a6c2e611c1458dcb357c122dc110725c40961
AlexDarthZ <36786432+alexdarthz@users.noreply.github.com>
2023-03-25 15:39:19 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const seatsContainer = document.getElementById('seats-container');
-const rows = 8;
-const cols = 8;
-let seatNumber = 1;
+const seatContainer = document.createElement('div');
+seatContainer.classList.add('seat-container');
+document.body.appendChild(seatContainer);
 
-for (let i = 0; i < rows; i++) {
+for (let i = 0; i < 8; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
+    seatContainer.appendChild(row);
 
-    for (let j = 0; j < cols; j++) {
+    for (let j = 0; j < 8; j++) {
         const seat = document.createElement('input');
         seat.type = 'button';
-        seat.value = seatNumber;
-        seatNumber++;
+        seat.value = `${i + 1}${j + 1}`;
         seat.classList.add('seat');
+        seat.onclick = function () {
+            console.log(this.value);
+            const popup = document.createElement('div');
+            popup.classList.add('popup');
+            popup.textContent = this.value;
+            document.body.appendChild(popup);
+            setTimeout(function () {
+                popup.remove();
+            }, 2000);
+        }
         row.appendChild(seat);
     }
-
-    seatsContainer.appendChild(row);
 }
